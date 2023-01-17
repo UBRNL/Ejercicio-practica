d64ab15c97492dd038835e3e4440e25c83df7299
Ubernel Hernandez Rios <conteraderiva22@gmail.com>
2023-01-17 09:44:27 -0500

    Ingreso js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function capturaDatos(){
+    var email = document.getElementById("exampleInputEmail1").value;
+    var password = document.getElementById("exampleInputPassword1").value;
+
+    var DatosRecopilados = [email, password];
+    console.log(DatosRecopilados)
+}
---
+++ js/indexRegistro.js
@@ -0,0 +1,23 @@
+function captura(){
+    var nombre = document.getElementById("validationDefault01").value;
+    var apellido = document.getElementById("validationDefault02").value;
+    var nombreUsario = document.getElementById("validationDefaultUsername").value;
+    var ciudad = document.getElementById("validationDefault03").value;
+    var estado = document.getElementById("validationDefault04").value;
+    var genero = document.getElementById("validationDefault05").value;
+    document.getElementById("validationDefault01").value;
+    document.getElementById("validationDefault02").value;
+    document.getElementById("validationDefaultUsername").value;
+    document.getElementById("validationDefault03").value;
+    document.getElementById("validationDefault04").value;
+    document.getElementById("validationDefault05").value;
+    document.getElementById("validationDefault01").focus;
+
+    var Datos = [nombre, apellido, nombreUsario, ciudad, estado, genero];
+    console.log(Datos)
+
+}
+
+
+
+
